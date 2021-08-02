d0bb602df101d6698f54f184bc1daad3c1d22ff9
liyanjun <254948133@qq.com>
2021-08-02 21:42:16 +0800

    下载

--- utils/package/lib/index.js
+++ utils/package/lib/index.js
@@ -1,14 +1,35 @@
 'use strict';
+const pathExists = require("path").sync
+const npminstall = require("npminstall");
 
-module.exports = Package;
+
 
 class Package {
     // TODO
-    constructor(targetPath, storePath, packageName, packageVersion){
+    constructor({targetPath, storePath, packageName, packageVersion}){
         this.targetPath = targetPath;
         this.storePath = storePath;
         this.packageName = packageName;
         this.packageVersion = packageVersion;
     }
-    // 
+    // 需要下载的包是否存在
+    exists(){
+        // 缓存目录加上规格化的包名
+        // if(pathExists("./package.json")){
+        //     console.log(111111111)
+        // }
+        console.log(this.storePath,this.targetPath, 111111111)
+        npminstall({
+            storePath: this.storePath,
+            pkgs: [
+                {
+                    name: this.packageName, 
+                    version: this.packageVersion
+                }
+            ],
+            root: this.targetPath,
+            registry: "https://registry.npmjs.org"
+        })
+    }
 }
+module.exports = Package;
